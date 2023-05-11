0b2668a1d85bde71a075f89931f4ed8065ec9ed0
BeatrizMoraes01 <annabeatrizdemoraes2424@gmail.com>
2023-05-11 20:46:19 -0300

    executavel

--- setup.py
+++
@@ -1,6 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name='Pokedex',
-      version='0.1',
-      description='Projeto Pokedex com Python',
-      executables=[Executable('Pokedex_Python.py')])
